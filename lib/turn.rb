4d38ea2fb8cf562249188ea0caa76ea95fec088c
Jackie First <jackiejeep@gmail.com>
2018-04-12 22:42:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,13 +8,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end 
 
-def turn(board)
-  puts "Please enter 1-9:"
-  until valid_move? == true 
-    turn(board)
-  end 
-end 
-
 def input_to_index(user_input)
   index = user_input.to_i-1
 end
@@ -28,3 +21,4 @@ end
 def move(board,index, token = 'X')
   board[index] = token
 end 
+
